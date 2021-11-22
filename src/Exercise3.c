ebdf246d4d48619794ac95d930c1a6e6d845983e
HoangphucNguyen1110 <80943005+HoangphucNguyen1110@users.noreply.github.com>
2021-11-22 17:53:58 +0700

    up e3 (2)

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -17,10 +17,13 @@ void Ex3(char *str){
 	//Your codes here
 	int i,j=0,k=0;
  	int a[15],max1,min1;
- 	
  	char str1[15][25];
-	for ( i=0;str[i]!= '\0';i++){
- 		if(str[i]=32){
+ 	printf("Input: ");
+ 	gets(str);
+ 	
+ 	for ( i=0;str[i]!= '\0';i++){
+ 		
+ 		if(str[i]==32){
  			
 			 str1[k][j]='\0';
  				k++;
@@ -28,20 +31,21 @@ void Ex3(char *str){
 		 }
 	
 		 else{
-		 	str1[j][k]=str[i];
+		 	str1[k][j]=str[i];
 		 	j++;
 		 }
 	 }
 	 str1[k][j]='\0';
-	 for ( i=0;i<k;j++){
+	 
+	 for ( i=0;i<=k;i++){
 	 	a[i]=strlen(str1[i]);
 	 }
 	 max1=a[0];
 	 min1=a[0];
-	 for(i=1;i<k;i++){
+	 for(i=1;i<=k;i++){
 	 	if(max1<a[i])
 	 	max1=a[i];
-	 	if(min1>a[i]);
+	 	if(min1>a[i])
 	 	min1=a[i];
 	 }
 	 for (i=0;i<=k;i++){
